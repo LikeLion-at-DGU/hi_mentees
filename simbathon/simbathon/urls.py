4d684d227c7ef1cc918ec1ae86594160f75e6f35
wonny7447 <wonny7447@naver.com>
2021-06-23 19:47:28 +0900

    hi-mentees 소개 페이지

--- simbathon/simbathon/urls.py
+++ simbathon/simbathon/urls.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+
+class IntroView(TemplateView):
+    template_name = 'introduction.html'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('mainPage.urls')),
     path('main/', include('mainPage.urls')),
+    path('introduction/', IntroView.as_view(), name='introduction'),
     path('lecture/', include('lecture.urls')),
     path('accounts/',include('allauth.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
