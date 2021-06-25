7196abc352e50b6d36e88898f85d08f463d6c2f8
wonny7447 <wonny7447@naver.com>
2021-06-26 00:31:33 +0900

    여원오류 페이지_mainPage 앱 작업

--- simbathon/mainPage/urls.py
+++ simbathon/mainPage/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import *
+from . import views
 
 app_name = "mainPage"
 urlpatterns = [
-    path('', main, name="main"),
+    #path('', main, name="main"),
+    path('', views.ShowLecture.as_view(), name="main"),
 ]
