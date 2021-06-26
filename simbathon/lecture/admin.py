93c7bf33663cc1a6b4199b03f8060bc2399b292f
minju <alswn9938@gmail.com>
2021-06-26 18:52:50 +0900

    [21.06.26 정민주]

--- simbathon/lecture/admin.py
+++ simbathon/lecture/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Category, Lecture
+from .models import Lecture
 # Register your models here.
 
-admin.site.register(Category)
+
 admin.site.register(Lecture)
--- simbathon/lecture/models.py
+++ simbathon/lecture/models.py
@@ -8,20 +8,19 @@ from django.conf import settings
 
 # Create your models here.
 
-class Category(models.Model):
-    name=models.CharField(max_length=20,null=False)
-
-    def __str__(self):
-        return self.name
-
-#카테고리 
-
 
 class Lecture(models.Model):
     id=models.AutoField(primary_key=True)
     title=models.CharField(max_length=200)
     #제목
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
+    CATEGORY_CHOICE=(
+        ('python','파이썬'),
+        ('ozobot','오조봇'),
+        ('entry','엔트리'),
+        ('goorum','구름'),
+        ('etc','기타'),
+    )
+    category = models.CharField(max_length=20,choices=CATEGORY_CHOICE, default="")
     #주제
     body=models.TextField()
     #내용
@@ -33,19 +32,24 @@ class Lecture(models.Model):
     ('star5','5단계'),)
     level=models.CharField(max_length=10, choices=LEVEL_CHOICE)
     #난이도
-    host=models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    #강사진 #강의:강사=1:n모델
+    host=models.ManyToManyField(UserProfile, blank=True, related_name="host")
+    #강사진 
     app_start_date=models.DateTimeField()
     #강의신청 시작일
     app_end_date=models.DateTimeField()
     #강의신청 마감일
     thumbnail=models.ImageField(upload_to='images/',blank=True, null=True)
     #강의 썸네일
+    lec_time=models.IntegerField(default=0)
+    #강의시간
+    webex_address=models.CharField(max_length=50, default="#")
+    #웹엑스 링크
     schedule=models.ImageField(upload_to='images/',blank=True, null=True)
     #시간표
     enrol_students=models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="enrol")
+    #신청한 사용자
+    like_users=models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="like_lectures")
     #좋아요한 사용자
-    like_users=models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_lectures")
     def __str__(self):
         return self.title #입력한 데이터가 호출되면 제목이 대표값으로 나오게 한다.
 
